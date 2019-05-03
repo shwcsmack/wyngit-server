301975f563cd5cb40d16d6d82440a4848e07d067
Shayne Stewart <shayne@code3dev.com>
2019-05-03 12:02:34 -0500

    added use of dotenv for environment variables and made custom ts type for it

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,39 @@
-import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as mongoose from "mongoose";
+import Controller from "./interfaces/controller.interface";
 
 class App {
   public app: express.Application;
-  public port: number;
 
-  constructor(controllers, port) {
+  constructor(controllers: Controller[]) {
     this.app = express();
-    this.port = port;
 
+    this.connectToTheDatabase();
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
+  }
+
+  public listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log(`App listening on the port ${process.env.PORT}`);
+    });
   }
 
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
   }
 
-  private initializeControllers(controllers) {
+  private initializeControllers(controllers: Controller[]) {
     controllers.forEach(controller => {
       this.app.use("/", controller.router);
     });
   }
 
-  public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+  private connectToTheDatabase() {
+    const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
+    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, {
+      useNewUrlParser: true,
     });
   }
 }
