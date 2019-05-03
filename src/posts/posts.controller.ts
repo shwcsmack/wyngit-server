ea2d1ac2c0b4de85b0f3ea8c7c80f7fad3eb16d6
Shayne Stewart <shayne@code3dev.com>
2019-05-03 12:38:05 -0500

    added REST routes for posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,35 +1,78 @@
 import * as express from "express";
+import Controller from "../interfaces/controller.interface";
 import Post from "./post.interface";
+import postModel from "./posts.model";
 
-class PostsController {
+class PostsController implements Controller {
   public path = "/posts";
   public router = express.Router();
-
-  private posts: Post[] = [
-    {
-      author: "Marcin",
-      content: "Dolor sit amet",
-      title: "Lorem Ipsum",
-    },
-  ];
+  private post = postModel;
 
   constructor() {
-    this.intializeRoutes();
+    this.initializeRoutes();
   }
 
-  public intializeRoutes() {
+  private initializeRoutes() {
     this.router.get(this.path, this.getAllPosts);
-    this.router.post(this.path, this.createAPost);
+    this.router.get(`${this.path}/:id`, this.getPostById);
+    this.router.put(`${this.path}/:id`, this.modifyPost);
+    this.router.delete(`${this.path}/:id`, this.deletePost);
+    this.router.post(this.path, this.createPost);
   }
 
-  getAllPosts = (request: express.Request, response: express.Response) => {
-    response.send(this.posts);
+  private getAllPosts = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    this.post.find().then(posts => {
+      response.send(posts);
+    });
   };
 
-  createAPost = (request: express.Request, response: express.Response) => {
-    const post: Post = request.body;
-    this.posts.push(post);
-    response.send(post);
+  private getPostById = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const id = request.params.id;
+    this.post.findById(id).then(post => {
+      response.send(post);
+    });
+  };
+
+  private modifyPost = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const id = request.params.id;
+    const postData: Post = request.body;
+    this.post.findByIdAndUpdate(id, postData, { new: true }).then(post => {
+      response.send(post);
+    });
+  };
+
+  private createPost = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const postData: Post = request.body;
+    const createdPost = new this.post(postData);
+    createdPost.save().then(savedPost => {
+      response.send(savedPost);
+    });
+  };
+
+  private deletePost = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const id = request.params.id;
+    this.post.findByIdAndDelete(id).then(successResponse => {
+      if (successResponse) {
+        response.send(200);
+      } else {
+        response.send(404);
+      }
+    });
   };
 }
 
