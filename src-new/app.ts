78442b1018baad8de7f6fe0033297d45aadf55c3
Shayne Stewart <sh.wc.smack@gmail.com>
2019-05-04 20:59:31 -0500

    Fixing linting and adding pre commit hooks

--- src-new/app.ts
+++ src-new/app.ts
@@ -1,22 +1,28 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
+// import * as bodyParser from 'body-parser';
 
 class App {
   public app: express.Application;
   public port: number;
 
-  constructor(port: number) {
+  public constructor(port: number) {
     this.app = express();
     this.port = port;
-    this.app.get('/', (req, res) => {
-      res.send('Hello World');
-    });
+    this.app.get(
+      '/',
+      (req, res): void => {
+        res.send('Hello World!!!!');
+      },
+    );
   }
 
-  public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on port ${this.port}`);
-    });
+  public listen(): void {
+    this.app.listen(
+      this.port,
+      (): void => {
+        console.log(`App listening on port ${this.port}`);
+      },
+    );
   }
 }
 
