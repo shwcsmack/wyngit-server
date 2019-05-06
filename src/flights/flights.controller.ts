40523159cc50dd9869f9b640d5fb4b032a504ca5
Shayne Stewart <sh.wc.smack@gmail.com>
2019-05-06 09:15:16 -0500

    hooked up mongoose

--- src/flights/flights.controller.ts
+++ src/flights/flights.controller.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, Application } from 'express';
+import { Router, Request, Response } from 'express';
 import Flight from './flight.interface';
 
 class FlightsController {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import App from './app';
 import FlightsController from './flights/flights.controller';
 
+import * as mongoose from 'mongoose';
+import 'dotenv/config';
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
+
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, { useNewUrlParser: true });
+
 const app = new App([new FlightsController()], 3000);
 
 app.listen();
